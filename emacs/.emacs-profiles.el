068d5bfdb5e405a3212fb39a6e1ba22350a571e0
Benj <Benjamin.Schwerdtner@gmail.com>
2021-07-05 09:50:54 +0200

    Add stuff

--- emacs/.emacs-profiles.el
+++ emacs/.emacs-profiles.el
@@ -1,4 +1,8 @@
-(("default" . ((user-emacs-directory . "~/.emacs-spacemacs.d"))))
+(("default" . ((user-emacs-directory . "~/.emacs-spacemacs.d")
+               (straight-p . t)))
+ ("mememacs" . ((user-emacs-directory . ".emacs-mememacs.d")
+                (straight-p . t)))
+ ("ambrevar" . ((user-emacs-directory . ".emacs-ambrevar.d"))))
 
 
 
