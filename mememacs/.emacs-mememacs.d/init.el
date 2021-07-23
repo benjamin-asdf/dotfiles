aca50af1f27ea77adaa3f7d29fa3a9cf8a3bc285
Benj <Benjamin.Schwerdtner@gmail.com>
2021-07-23 15:05:59 +0200

    Add initial mememacs

---
+++ mememacs/.emacs-mememacs.d/init.el
@@ -0,0 +1,84 @@
+
+;;; Speed up init.
+;;; Temporarily reduce garbage collection during startup. Inspect `gcs-done'.
+(defun ambrevar/reset-gc-cons-threshold ()
+  (setq gc-cons-threshold (car (get 'gc-cons-threshold 'standard-value))))
+(setq gc-cons-threshold (* 64 1024 1024))
+(add-hook 'after-init-hook 'ambrevar/reset-gc-cons-threshold)
+;;; Temporarily disable the file name handler.
+(setq default-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+(defun ambrevar/reset-file-name-handler-alist ()
+  (setq file-name-handler-alist default-file-name-handler-alist))
+(add-hook 'after-init-hook 'ambrevar/reset-file-name-handler-alist)
+
+
+;;; Avoid the "loaded old bytecode instead of newer source" pitfall.
+(setq load-prefer-newer t)
+
+
+;;; Store additional config in a 'lisp' subfolder and add it to the load path so
+;;; that `require' can find the files.
+;;; This must be done before moving `user-emacs-directory'.
+(add-to-list 'load-path (expand-file-name "lisp/" user-emacs-directory))
+
+
+
+
+(setq inhibit-startup-message t)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+(menu-bar-mode -1)
+(setq visible-bell 1)
+
+
+(require 'use-package)
+(setq use-package-verbose t)
+
+
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+	              treemacs-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+
+
+;; ;;; Local config.  See below for an example usage.
+;; (load "local-before" t)
+
+(require 'functions)
+(require 'main)
+(require 'visual)
+
+(setf straight-use-package-by-default t)
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+
+(use-package helm
+  :demand t
+  :config (global-set-key (kbd "M-x") 'helm-M-x))
+
