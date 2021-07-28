0f8320d1a3bc3704047cac8005f046490960a06e
Benj <Benjamin.Schwerdtner@gmail.com>
2021-07-28 12:28:29 +0200

    Fix profiles

--- gaia-s-emacs-profiles/.emacs-profiles.el
+++ gaia-s-emacs-profiles/.emacs-profiles.el
@@ -1,6 +1,6 @@
 (("default" . ((user-emacs-directory . "~/.emacs-spacemacs.d")
                (straight-p . t)))
- ("mememacs" . ((user-emacs-directory . ".emacs-mememacs.d")
+ ("mememacs" . ((user-emacs-directory . "~/.emacs-mememacs.d")
                 (straight-p . t)))
  ("ambrevar" . ((user-emacs-directory . ".emacs-ambrevar.d")))
  ("doom" . ((user-emacs-directory . ".emacs-doom.d")
