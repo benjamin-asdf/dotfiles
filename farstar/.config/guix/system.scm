e472d9aea9b04f970fc45e98495a801e57ecd0da
Benj <Benjamin.Schwerdtner@gmail.com>
2021-07-28 20:36:16 +0200

    Add system config

---
+++ farstar/.config/guix/system.scm
@@ -0,0 +1,52 @@
+;; This is an operating system configuration generated
+;; by the graphical installer.
+
+(use-modules (gnu) (nongnu packages linux))
+(use-service-modules desktop networking ssh xorg)
+
+(operating-system
+ (kernel linux)
+ (firmware (list linux-firmware))
+  (locale "en_US.utf8")
+  (timezone "Europe/Berlin")
+  (keyboard-layout (keyboard-layout "us"))
+  (host-name "farstar")
+  (users (cons* (user-account
+                  (name "benj")
+                  (comment "Benjamin")
+                  (group "users")
+                  (home-directory "/home/benj")
+                  (supplementary-groups
+                    '("wheel" "netdev" "audio" "video")))
+                %base-user-accounts))
+  (packages
+    (append
+      (list (specification->package "emacs")
+            (specification->package "emacs-exwm")
+            (specification->package
+              "emacs-desktop-environment")
+            (specification->package "nss-certs"))
+      %base-packages))
+  (services %desktop-services)
+  (bootloader
+    (bootloader-configuration
+      (bootloader grub-efi-bootloader)
+      (target "/boot/efi")
+      (keyboard-layout keyboard-layout)))
+  (mapped-devices
+    (list (mapped-device
+            (source
+              (uuid "5c25cd91-35cb-4a74-99f9-91f1a737a280"))
+            (target "cryptroot")
+            (type luks-device-mapping))))
+  (file-systems
+    (cons* (file-system
+             (mount-point "/boot/efi")
+             (device (uuid "BE2B-23AE" 'fat32))
+             (type "vfat"))
+           (file-system
+             (mount-point "/")
+             (device "/dev/mapper/cryptroot")
+             (type "ext4")
+             (dependencies mapped-devices))
+           %base-file-systems)))
