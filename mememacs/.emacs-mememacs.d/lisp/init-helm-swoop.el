7af5f7c4c6648a687fd0c009a618e59b4b2f9420
Benj <Benjamin.Schwerdtner@gmail.com>
2021-09-14 13:04:48 +0200

    Delete cruft

--- mememacs/.emacs-mememacs.d/lisp/init-helm-swoop.el
+++ mememacs/.emacs-mememacs.d/lisp/init-helm-swoop.el
@@ -13,13 +13,6 @@
        (buffer-substring (point) (point-max))))
    arg))
 
-(defun mememacs/helm-do-ag-file ()
-  (interactive)
-  (if (buffer-file-name)
-      (helm-do-ag
-       default-directory
-       (list (buffer-file-name)))))
-
 
 (defadvice helm-swoop (around my/helm-swoop-advice (&rest args) activate)
   (if (and
@@ -27,7 +20,7 @@
        (> (line-number-at-pos (point-max)) 5000))
       (progn
         (message "Using rg instead of swoop in big buffer.")
-        (mememacs/helm-do-ag-file))
+	(helm-ag-this-file))
     ad-do-it))
 
 (define-key helm-swoop-map (kbd "C-c C-o") #'mememacs/helm-swoop-kill-line-and-quit)
