04a5ac14c8328054a4b239503f29db96cf4d70af
Benj <Benjamin.Schwerdtner@gmail.com>
2022-03-15 12:40:41 +0100

    Add dunst and some stumpwm config

--- gaia-s-stumpwm/.stumpwm.d/init.lisp
+++ gaia-s-stumpwm/.stumpwm.d/init.lisp
@@ -19,12 +19,17 @@ Load a file that re-defines swank and then calls it."
 (bind "l" "move-focus right")
 (bind "h" "move-focus left")
 (bind "d" "kill")
+(bind "C-h" '*help-map*)
+
+(setf  *help-keys* '("?" "H-h"))
 
 (define-key *top-map* (kbd "s-f") "fullscreen")
 (define-key *top-map* (kbd "s-w") "fselect")
 (define-key *top-map* (kbd "s-d") "delete-window")
 (define-key *top-map* (kbd "s-o") "only")
 (define-key *top-map* (kbd "s-e") "emacs")
+(define-key *top-map* (kbd "s-m") "mode-line")
+(define-key *top-map* (kbd "s-i") "pull-from-windowlist")
 
 (defcommand flameshot-gui () ()
   (run-shell-command "flameshot gui"))
@@ -38,3 +43,51 @@ Load a file that re-defines swank and then calls it."
 (defcommand lock () ()
   (run-shell-command "best-lock.sh"))
 (define-key *top-map* (kbd "s-9")  "lock")
+
+
+(Load-module "cpu")
+
+(setf
+ *screen-mode-line-format*
+ "[^B%n^b] %W %C")
+
+(load-module "pass")
+(define-key *top-map* (kbd "s-a") "pass-copy")
+
+(defcommand kill-unclutter () ()
+  (run-shell-command "pkill unclutter"))
+(defcommand start-unclutter () ()
+  (run-shell-command "unclutter &"))
+
+(define-interactive-keymap
+    normie-mode
+    (:on-enter #'kill-unclutter
+     :on-exit #'start-unclutter)
+  ((kbd "J") "ratrelwarp  0 +5")
+  ((kbd "j") "ratrelwarp  0 +30")
+  ((kbd "K") "ratrelwarp  0 -5")
+  ((kbd "k") "ratrelwarp  0 -30")
+  ((kbd "H") "ratrelwarp -5  0")
+  ((kbd "h") "ratrelwarp -30  0")
+  ((kbd "L") "ratrelwarp +5  0")
+  ((kbd "l") "ratrelwarp +30  0")
+  ((kbd "d") "ratclick 1")
+  ((kbd "c") "ratclick 3"))
+
+(define-key *top-map* (kbd "s-;") "normie-mode")
+
+
+(push '(:class "Unity-editor") *deny-raise-request*)
+(push '(:class "Unity-editor") *deny-map-request*)
+
+(defmacro comment (&rest body))
+
+
+
+
+
+
+
+(comment
+ (setf *debug-level* 1)
+ (redirect-all-output (data-dir-file "output" "log")))
