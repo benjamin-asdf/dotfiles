5d55912f317121a9a2e9774687234a0ec7737bc3
Benj <Benjamin.Schwerdtner@gmail.com>
2022-05-01 09:46:21 +0200

    Add init-project

---
+++ mememacs/.emacs-mememacs.d/lisp/init-project.el
@@ -0,0 +1,36 @@
+;; -*- lexical-binding: t; -*-
+
+(mememacs/leader-def
+  "p"
+  project-prefix-map
+  "p."
+  (defun mm/project-list-file ()
+    (interactive)
+    (find-file project-list-file)))
+
+(defun mememacs/fd-find-file ()
+  (interactive)
+  (find-file
+   (completing-read
+    "find file: "
+    (let ((command "fd --hidden --exclude=.git --type=f . --print0"))
+      (with-temp-buffer
+	(shell-command
+	 command
+	 t
+	 "*project-files-errors*")
+	(let ((shell-output (buffer-substring
+			     (point-min)
+			     (point-max))))
+	  (split-string
+	   (string-trim shell-output)
+	   "\0"
+	   t)))))))
+
+(general-def "s-\\" #'mememacs/fd-find-file)
+
+(add-to-list
+ 'project-switch-commands
+ '(project-dired "Dired"))
+
+(provide 'init-project)
