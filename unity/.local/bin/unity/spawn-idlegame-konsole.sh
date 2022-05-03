630cf33f06731a01edcc91615f5f743fecbae2f1
Benj <Benjamin.Schwerdtner@gmail.com>
2022-05-03 09:56:51 +0200

    Simplify start  script

--- unity/.local/bin/unity/spawn-idlegame-konsole.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-IDLEGAMEDIR=~/idlegame/IdleGame/
-[ -n "$IDLEGAMEDIR" ] && rm -rf "$IDLEGAMEDIR/Temp/*"
-
-xterm -T "idlegame-unity" -e sh -c "unity-editor -forcefree -buildTarget standalone -projectPath $IDLEGAMEDIR" &
