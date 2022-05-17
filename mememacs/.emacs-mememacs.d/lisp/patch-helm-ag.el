049d7adc60bdc57ec813d47a99a2f092fed492f1
Benj <Benjamin.Schwerdtner@gmail.com>
2022-05-17 11:10:24 +0200

    Delete some helm stuff

--- mememacs/.emacs-mememacs.d/lisp/patch-helm-ag.el
+++
@@ -1,6 +0,0 @@
-(defun mm/filter-file-exists (lst)
-  (-filter #'file-exists-p lst))
-
-(advice-add #'helm-ag--file-visited-buffers :filter-return #'mm/filter-file-exists)
-
-(provide 'patch-helm-ag)
