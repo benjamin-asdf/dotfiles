8fd18ff6bd2a1ed2379e53e26282f01dcc397e44
Benni Schwerdtner <Benjamin.Schwerdtner@gmail.com>
2023-05-14 18:23:35 +0200

    Curr

--- scripts/.local/bin/scripts/battery-overview.sh
+++ scripts/.local/bin/scripts/battery-overview.sh
@@ -4,4 +4,4 @@ percentage=$(battery-percentage.sh)
 hours_left=$(battery-hours.sh)
 charging_status=$(cat /sys/class/power_supply/BAT0/status)
 
-notify-send "Battery Overview" "[|||] Percentage: $percentage\nTime remaining: $hours_left\nCharging Status: $charging_status"
+notify-send "Battery [|||]" "Percentage: $percentage\nTime remaining: $hours_left\nCharging Status: $charging_status"
